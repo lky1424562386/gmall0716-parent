e5472056c1746877888b1fc0a052d78e577df0d1
lky1424562386 <1424562386@qq.com>
2020-09-18 21:19:46 +0800

    上传

---
+++ gmall0716-common/src/main/java/com/lky/gmall0716/common/constant/GmallConstant.java
@@ -0,0 +1,7 @@
+package com.lky.gmall0716.common.constant;
+
+public class GmallConstant {
+    public static final String KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
+    public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
+
+}
