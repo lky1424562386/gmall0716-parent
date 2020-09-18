46ac3f0a3fb358894923e5004a40b18a46ed8e4a
李开元 <1424562386@qq.com>
2020-09-18 22:56:50 +0800

    修改

--- gmall0716-common/src/main/java/com/lky/gmall0716/common/constant/GmallConstant.java
+++ gmall0716-common/src/main/java/com/lky/gmall0716/common/constant/GmallConstant.java
@@ -1,6 +1,9 @@
 package com.lky.gmall0716.common.constant;
 
 public class GmallConstant {
+    public static void main(String[] args) {
+        System.out.println("saaaaaaaaaa");
+    }
     public static final String KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
     public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
 
